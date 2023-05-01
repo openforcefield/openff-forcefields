c37f4d3b07651021471ddefd32e5b563042704c5
Chapin Cavender <chapincavender@gmail.com>
2023-05-01 11:16:08 -0700

    OPC water and ion parameters

---
+++ scripts/get_ion_nb_params_from_ambertools_frcmod.py
@@ -0,0 +1,44 @@
+import click
+import pandas
+from pathlib import Path
+
+@click.command()
+@click.argument(
+    "amber_frcmod_path",
+    type = click.Path(),
+)
+def main(amber_frcmod_path):
+
+    ion_nb_params = list()
+
+    nonbonded = False
+
+    with open(Path(amber_frcmod_path), "r") as amber_frcmod_file:
+        for line in amber_frcmod_file:
+
+            if line == '\n':
+                nonbonded = False
+
+            if nonbonded:
+
+                fields = line.split()
+
+                ion_nb_params.append({
+                    "element": fields[0],
+                    "opc_rmin2 (A)": float(fields[1]),
+                    "opc_eps (kcal/mol)": float(fields[2]),
+                })
+
+            if line.startswith('NONBON'):
+                nonbonded = True
+
+    output_path = Path(
+        "openforcefields", "data", f"{Path(amber_frcmod_path).suffix[1:]}.csv"
+    )
+    pandas.DataFrame(ion_nb_params).set_index("element").to_csv(output_path)
+
+
+if __name__ == "__main__":
+    main()
+
+
