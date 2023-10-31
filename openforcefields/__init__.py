3f04f45b75eb786a007726e0bce16e7160fb86fa
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-10-31 09:54:04 -0500

    Remove duplicate code

--- openforcefields/__init__.py
+++ openforcefields/__init__.py
@@ -1,16 +1,7 @@
 """
-Open Force fields
+OpenFF Force fields
 """
-
-# Add imports here
-from .openforcefields import get_forcefield_dirs_paths
-
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+from openforcefields.openforcefields import get_forcefield_dirs_paths
 
 from . import _version
 __version__ = _version.get_versions()['version']
