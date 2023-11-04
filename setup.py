529729786d1a68a709705eed1a4d1b169cd2687e
Matt Thompson <mattwthompson@protonmail.com>
2023-11-03 21:00:31 -0500

    Update `setup.py` (#88)

--- setup.py
+++ setup.py
@@ -1,28 +1,20 @@
 """
-openforcefields
+openff-forcefields
 """
-import sys
 from setuptools import setup
 import versioneer
 
 short_description = __doc__.split("\n")
 
-# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
-
-try:
-    with open("README.md", "r") as handle:
-        long_description = handle.read()
-except:
-    long_description = "\n".join(short_description[2:]),
+with open("README.md", "r") as handle:
+    long_description = handle.read()
 
 
 setup(
     # Self-descriptive entries which should always be present
     name='openforcefields',
     author='The Open Force Field Initiative',
-    author_email='dmobley@uci.edu',
+    author_email='info@openforcefield.org',
     description=short_description[0],
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -39,19 +31,11 @@ setup(
     package_data={'openforcefields': ["offxml/*"]
                   },
 
-    # Allows `setup.py test` to work correctly with pytest
-    setup_requires=[] + pytest_runner,
-
     # Additional entries you may want simply uncomment the lines you want and fill in the data
-    url='https://github.com/openforcefield/openforcefields',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    url='https://github.com/openforcefield/openforcefields',
     platforms=['Linux',
                'Mac OS-X',
                'Unix'],                 # Valid platforms your code works on, adjust to your flavor
-    # python_requires=">=3.5",          # Python version restrictions
-
-    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
-    # zip_safe=False,
 
     # Add entry point so that the forcefield directory can be discovered by the openforcefield toolkit.
     entry_points={
