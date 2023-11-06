9cecc13f8b43606c8081e28b12acd229b9ef6b6a
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-11-06 14:18:14 -0600

    Fix bad merge

--- openforcefields/tests/test_water_models.py
+++ openforcefields/tests/test_water_models.py
@@ -405,16 +405,6 @@ def test_tip4p_ew(water_molecule):
         },
     )
 
-    compare_water_systems(
-        reference,
-        system,
-        {
-            "charge": 1e-10 * openmm.unit.elementary_charge,
-            "sigma": 1e-10 * openmm.unit.nanometer,
-            "epsilon": 1e-10 * openmm.unit.kilojoule_per_mole,
-        },
-    )
-
 
 def test_tip5p(water_molecule, tip4p_positions):
     from openmm.app import Modeller
