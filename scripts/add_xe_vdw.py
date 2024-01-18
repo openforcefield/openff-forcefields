8b2070c52610d8fa2a870755ffb86de13a573309
Alexandra McIsaac <armcisaac@gmail.com>
2024-01-18 09:47:42 -0800

    Update add_xe_vdw.py

--- scripts/add_xe_vdw.py
+++ scripts/add_xe_vdw.py
@@ -18,7 +18,7 @@ xe_param = vdWHandler.vdWType(
 
 sage_21.get_parameter_handler("vdW").parameters.append(xe_param)
 sage_21.to_file("../openforcefields/offxml/openff-2.1.1.offxml")
-sage_21_uc.get_parameter_handler("vdW").parameters.append(xe_idx_sage21, xe_param)
+sage_21_uc.get_parameter_handler("vdW").parameters.append(xe_param)
 sage_21_uc.to_file("../openforcefields/offxml/openff_unconstrained-2.1.1.offxml")
 
 # Make sure new FF loads with most recent toolkit version
